7dd98d92b1537a43d0b43798e4e566a2b4b5b5c6
noisenuisance <sabauspne@gmail.com>
2022-10-01 01:22:45 +0300

    feat: add routes & auth middleware

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+const auth = require('../middlewares/auth');
+
+const userRouter = require('./users');
+const movieRouter = require('./movies');
+const { createUser, login } = require('../controllers/users');
+
+const NotFoundError = require('../helpers/errors/NotFoundError');
+const { pageNotFoundMessage } = require('../helpers/constants');
+// const { validateLogin, validateCreateUser } = require('../middlewares/validation');
+
+router.post('/signup', createUser);
+router.post('/signin', login);
+
+router.use(auth);
+
+router.use(userRouter);
+router.use(movieRouter);
+
+router.use((req, res, next) => next(new NotFoundError(pageNotFoundMessage)));
+
+module.exports = router;
